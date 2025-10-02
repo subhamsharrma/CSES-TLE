d153d164a9c23e31a0dfcfb75c89612130aa31cc
subham sharrma <subhamsharrma@gmail.com>
2025-10-02 22:55:08 +0530

    updatedcode

--- A_Young_Physicist.cpp
+++ A_Young_Physicist.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-int main () {
 
-    int x , y , z ;
-    cin >> x >> y >> z ;
-    
-    return 0 ; 
+int main() {
+    int n;
+    cin >> n;
+
+    int sum_x = 0, sum_y = 0, sum_z = 0;
+
+    for(int i = 0; i < n; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;      // <-- take the items here
+        sum_x += x;
+        sum_y += y;
+        sum_z += z;
+    }
+
+    if(sum_x == 0 && sum_y == 0 && sum_z == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
 }
