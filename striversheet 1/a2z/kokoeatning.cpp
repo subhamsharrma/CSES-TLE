b3138bdc7195753b6c4cbfcede03940c3353d7d0
subham sharrma <subhamsharrma@gmail.com>
2025-10-03 16:44:34 +0530

    Revisiti

--- striversheet 1/a2z/kokoeatning.cpp
+++ striversheet 1/a2z/kokoeatning.cpp
@@ -1,34 +1,34 @@
-// Example 1:
+// // Example 1:
 
-// Input: piles = [3,6,7,11], h = 8
-// Output: 4
-// Example 2:
+// // Input: piles = [3,6,7,11], h = 8
+// // Output: 4
+// // Example 2:
 
-// Input: piles = [30,11,23,4,20], h = 5
-// Output: 30
+// // Input: piles = [30,11,23,4,20], h = 5
+// // Output: 30
 
 
-class Solution {
-public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        int ans = right;
+// class Solution {
+// public:
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         int left = 1, right = *max_element(piles.begin(), piles.end());
+//         int ans = right;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
             
-            long long hours = 0;
-            for (int pile : piles) {
-                hours += (pile + mid - 1) / mid; // ceil(pile/mid)
-            }
+//             long long hours = 0;
+//             for (int pile : piles) {
+//                 hours += (pile + mid - 1) / mid; // ceil(pile/mid)
+//             }
             
-            if (hours <= h) {
-                ans = mid;      // possible answer
-                right = mid - 1; // try smaller
-            } else {
-                left = mid + 1;  // need bigger speed
-            }
-        }
-        return ans;
-    }
-};
+//             if (hours <= h) {
+//                 ans = mid;      // possible answer
+//                 right = mid - 1; // try smaller
+//             } else {
+//                 left = mid + 1;  // need bigger speed
+//             }
+//         }
+//         return ans;
+//     }
+// };
