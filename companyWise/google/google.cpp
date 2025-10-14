7193a7d77454a4de61e4452f50e60d1d6953ae92
Subham Sharma <subhamsharrma@gmail.com>
2025-10-14 13:34:06 +0530

    NewQue

---
+++ companyWise/google/google.cpp
@@ -0,0 +1,203 @@
+
+// https://github.com/liquidslr/leetcode-company-wise-problems/tree/main/Google
+// I'll format the LeetCode problems with their names and links:
+
+// Two Sum --- > https://leetcode.com/problems/two-sum
+
+// Add Two Numbers --- > https://leetcode.com/problems/add-two-numbers
+
+// Trapping Rain Water --- > https://leetcode.com/problems/trapping-rain-water
+
+// Median of Two Sorted Arrays --- > https://leetcode.com/problems/median-of-two-sorted-arrays
+
+// Longest Substring Without Repeating Characters --- > https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+// Longest Common Prefix --- > https://leetcode.com/problems/longest-common-prefix
+
+// Valid Parentheses --- > https://leetcode.com/problems/valid-parentheses
+
+// Longest Consecutive Sequence --- > https://leetcode.com/problems/longest-consecutive-sequence
+
+// Maximum Subarray --- > https://leetcode.com/problems/maximum-subarray
+
+// Rotate Image --- > https://leetcode.com/problems/rotate-image
+
+// Search in Rotated Sorted Array --- > https://leetcode.com/problems/search-in-rotated-sorted-array
+
+// Reverse Linked List --- > https://leetcode.com/problems/reverse-linked-list
+
+// 4Sum --- > https://leetcode.com/problems/4sum
+
+// Jump Game --- > https://leetcode.com/problems/jump-game
+
+// Group Anagrams --- > https://leetcode.com/problems/group-anagrams
+
+// Find Median from Data Stream --- > https://leetcode.com/problems/find-median-from-data-stream
+
+// Pascal's Triangle --- > https://leetcode.com/problems/pascals-triangle
+
+// Single Number --- > https://leetcode.com/problems/single-number
+
+// Find the Index of the First Occurrence in a String --- > https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+// Maximal Rectangle --- > https://leetcode.com/problems/maximal-rectangle
+
+// Valid Anagram --- > https://leetcode.com/problems/valid-anagram
+
+// First Missing Positive --- > https://leetcode.com/problems/first-missing-positive
+
+// Jump Game II --- > https://leetcode.com/problems/jump-game-ii
+
+// Add Binary --- > https://leetcode.com/problems/add-binary
+
+// Text Justification --- > https://leetcode.com/problems/text-justification
+
+// Insert Interval --- > https://leetcode.com/problems/insert-interval
+
+// Maximum Product Subarray --- > https://leetcode.com/problems/maximum-product-subarray
+
+// String to Integer (atoi) --- > https://leetcode.com/problems/string-to-integer-atoi
+
+// Same Tree --- > https://leetcode.com/problems/same-tree
+
+// Search a 2D Matrix --- > https://leetcode.com/problems/search-a-2d-matrix
+
+// Isomorphic Strings --- > https://leetcode.com/problems/isomorphic-strings
+
+// 3Sum Closest --- > https://leetcode.com/problems/3sum-closest
+
+// Wildcard Matching --- > https://leetcode.com/problems/wildcard-matching
+
+// Find the Duplicate Number --- > https://leetcode.com/problems/find-the-duplicate-number
+
+// Contains Duplicate II --- > https://leetcode.com/problems/contains-duplicate-ii
+
+// Missing Number --- > https://leetcode.com/problems/missing-number
+
+// Symmetric Tree --- > https://leetcode.com/problems/symmetric-tree
+
+// Minimum Path Sum --- > https://leetcode.com/problems/minimum-path-sum
+
+// Kth Smallest Element in a BST --- > https://leetcode.com/problems/kth-smallest-element-in-a-bst
+
+// Copy List with Random Pointer --- > https://leetcode.com/problems/copy-list-with-random-pointer
+
+// Number of Islands II --- > https://leetcode.com/problems/number-of-islands-ii
+
+// Clone Graph --- > https://leetcode.com/problems/clone-graph
+
+// Integer to Roman --- > https://leetcode.com/problems/integer-to-roman
+
+// Shortest Palindrome --- > https://leetcode.com/problems/shortest-palindrome
+
+// Flatten Binary Tree to Linked List --- > https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+
+// Group Shifted Strings --- > https://leetcode.com/problems/group-shifted-strings
+
+// Word Search II --- > https://leetcode.com/problems/word-search-ii
+
+// Binary Tree Right Side View --- > https://leetcode.com/problems/binary-tree-right-side-view
+
+// Subsets II --- > https://leetcode.com/problems/subsets-ii
+
+// The Skyline Problem --- > https://leetcode.com/problems/the-skyline-problem
+
+// Implement Stack using Queues --- > https://leetcode.com/problems/implement-stack-using-queues
+
+// Best Time to Buy and Sell Stock with Cooldown --- > https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown
+
+// Missing Ranges --- > https://leetcode.com/problems/missing-ranges
+
+// Graph Valid Tree --- > https://leetcode.com/problems/graph-valid-tree
+
+// Second Highest Salary --- > https://leetcode.com/problems/second-highest-salary
+
+// Basic Calculator II --- > https://leetcode.com/problems/basic-calculator-ii
+
+// Game of Life --- > https://leetcode.com/problems/game-of-life
+
+// Fraction to Recurring Decimal --- > https://leetcode.com/problems/fraction-to-recurring-decimal
+
+// Number of Digit One --- > https://leetcode.com/problems/number-of-digit-one
+
+// Walls and Gates --- > https://leetcode.com/problems/walls-and-gates
+
+// Binary Tree Paths --- > https://leetcode.com/problems/binary-tree-paths
+
+// Wiggle Sort --- > https://leetcode.com/problems/wiggle-sort
+
+// Best Time to Buy and Sell Stock IV --- > https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv
+
+// 3Sum Smaller --- > https://leetcode.com/problems/3sum-smaller
+
+// Combinations --- > https://leetcode.com/problems/combinations
+
+// Lowest Common Ancestor of a Binary Search Tree --- > https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+
+// Read N Characters Given read4 II - Call Multiple Times --- > https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times
+
+// Paint Fence --- > https://leetcode.com/problems/paint-fence
+
+// Longest Substring with At Most Two Distinct Characters --- > https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters
+
+// Flip Game --- > https://leetcode.com/problems/flip-game
+
+// Binary Tree Postorder Traversal --- > https://leetcode.com/problems/binary-tree-postorder-traversal
+
+// Strobogrammatic Number --- > https://leetcode.com/problems/strobogrammatic-number
+
+// Zigzag Iterator --- > https://leetcode.com/problems/zigzag-iterator
+
+// Spiral Matrix II --- > https://leetcode.com/problems/spiral-matrix-ii
+
+// Peeking Iterator --- > https://leetcode.com/problems/peeking-iterator
+
+// Department Top Three Salaries --- > https://leetcode.com/problems/department-top-three-salaries
+
+// Closest Binary Search Tree Value II --- > https://leetcode.com/problems/closest-binary-search-tree-value-ii
+
+// Flatten 2D Vector --- > https://leetcode.com/problems/flatten-2d-vector
+
+// Unique Word Abbreviation --- > https://leetcode.com/problems/unique-word-abbreviation
+
+// Flip Game II --- > https://leetcode.com/problems/flip-game-ii
+
+// Palindrome Permutation --- > https://leetcode.com/problems/palindrome-permutation
+
+// Scramble String --- > https://leetcode.com/problems/scramble-string
+
+// Insertion Sort List --- > https://leetcode.com/problems/insertion-sort-list
+
+// Factorial Trailing Zeroes --- > https://leetcode.com/problems/factorial-trailing-zeroes
+
+// Remove Duplicates from Sorted List II --- > https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
+
+// Populating Next Right Pointers in Each Node --- > https://leetcode.com/problems/populating-next-right-pointers-in-each-node
+
+// Word Pattern --- > https://leetcode.com/problems/word-pattern
+
+// N-Queens II --- > https://leetcode.com/problems/n-queens-ii
+
+// Minimum Depth of Binary Tree --- > https://leetcode.com/problems/minimum-depth-of-binary-tree
+
+// Bitwise AND of Numbers Range --- > https://leetcode.com/problems/bitwise-and-of-numbers-range
+
+// Rank Scores --- > https://leetcode.com/problems/rank-scores
+
+// Valid Number --- > https://leetcode.com/problems/valid-number
+
+// Read N Characters Given Read4 --- > https://leetcode.com/problems/read-n-characters-given-read4
+
+// Gray Code --- > https://leetcode.com/problems/gray-code
+
+// Remove Invalid Parentheses --- > https://leetcode.com/problems/remove-invalid-parentheses
+
+// Find Minimum in Rotated Sorted Array II --- > https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii
+
+// Find the Celebrity --- > https://leetcode.com/problems/find-the-celebrity
+
+// Range Sum Query - Mutable --- > https://leetcode.com/problems/range-sum-query-mutable
+
+// Binary Tree Level Order Traversal II --- > https://leetcode.com/problems/binary-tree-level-order-traversal-ii
+
+// Paint House --- > https://leetcode.com/problems/paint-house
---
+++ companyWise/walmart/walmart.cpp
@@ -0,0 +1,203 @@
+// https://github.com/liquidslr/leetcode-company-wise-problems/tree/main/Google
+
+// Here's the formatted list from the second document:
+
+// Longest Substring Without Repeating Characters --- > https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+// LRU Cache --- > https://leetcode.com/problems/lru-cache
+
+// Valid Parentheses --- > https://leetcode.com/problems/valid-parentheses
+
+// Max Stack --- > https://leetcode.com/problems/max-stack
+
+// Merge Intervals --- > https://leetcode.com/problems/merge-intervals
+
+// Trapping Rain Water --- > https://leetcode.com/problems/trapping-rain-water
+
+// Best Time to Buy and Sell Stock --- > https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+// Two Sum --- > https://leetcode.com/problems/two-sum
+
+// Longest Palindromic Substring --- > https://leetcode.com/problems/longest-palindromic-substring
+
+// Word Break --- > https://leetcode.com/problems/word-break
+
+// Divide Intervals Into Minimum Number of Groups --- > https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups
+
+// Minimum Number of Operations to Make Arrays Similar --- > https://leetcode.com/problems/minimum-number-of-operations-to-make-arrays-similar
+
+// Count Good Triplets in an Array --- > https://leetcode.com/problems/count-good-triplets-in-an-array
+
+// Minimum Operations to Make Array Equal II --- > https://leetcode.com/problems/minimum-operations-to-make-array-equal-ii
+
+// Maximum Number of Tasks You Can Assign --- > https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign
+
+// Middle of the Linked List --- > https://leetcode.com/problems/middle-of-the-linked-list
+
+// Number of Ways to Separate Numbers --- > https://leetcode.com/problems/number-of-ways-to-separate-numbers
+
+// The Winner University --- > https://leetcode.com/problems/the-winner-university
+
+// Generate Parentheses --- > https://leetcode.com/problems/generate-parentheses
+
+// Kth Largest Element in an Array --- > https://leetcode.com/problems/kth-largest-element-in-an-array
+
+// Search in Rotated Sorted Array --- > https://leetcode.com/problems/search-in-rotated-sorted-array
+
+// Group Anagrams --- > https://leetcode.com/problems/group-anagrams
+
+// Number of Islands --- > https://leetcode.com/problems/number-of-islands
+
+// Spiral Matrix --- > https://leetcode.com/problems/spiral-matrix
+
+// Longest Consecutive Sequence --- > https://leetcode.com/problems/longest-consecutive-sequence
+
+// Sort Colors --- > https://leetcode.com/problems/sort-colors
+
+// Valid Sudoku --- > https://leetcode.com/problems/valid-sudoku
+
+// Coin Change --- > https://leetcode.com/problems/coin-change
+
+// Binary Tree Zigzag Level Order Traversal --- > https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
+
+// Move Zeroes --- > https://leetcode.com/problems/move-zeroes
+
+// Combination Sum --- > https://leetcode.com/problems/combination-sum
+
+// Rotate Array --- > https://leetcode.com/problems/rotate-array
+
+// Degree of an Array --- > https://leetcode.com/problems/degree-of-an-array
+
+// All Nodes Distance K in Binary Tree --- > https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree
+
+// 3Sum --- > https://leetcode.com/problems/3sum
+
+// Top K Frequent Elements --- > https://leetcode.com/problems/top-k-frequent-elements
+
+// Pow(x, n) --- > https://leetcode.com/problems/powx-n
+
+// Median of Two Sorted Arrays --- > https://leetcode.com/problems/median-of-two-sorted-arrays
+
+// House Robber --- > https://leetcode.com/problems/house-robber
+
+// LFU Cache --- > https://leetcode.com/problems/lfu-cache
+
+// Flatten Nested List Iterator --- > https://leetcode.com/problems/flatten-nested-list-iterator
+
+// Reverse Nodes in k-Group --- > https://leetcode.com/problems/reverse-nodes-in-k-group
+
+// Longest Common Prefix --- > https://leetcode.com/problems/longest-common-prefix
+
+// Daily Temperatures --- > https://leetcode.com/problems/daily-temperatures
+
+// Merge k Sorted Lists --- > https://leetcode.com/problems/merge-k-sorted-lists
+
+// First Missing Positive --- > https://leetcode.com/problems/first-missing-positive
+
+// Word Search --- > https://leetcode.com/problems/word-search
+
+// Add Binary --- > https://leetcode.com/problems/add-binary
+
+// Copy List with Random Pointer --- > https://leetcode.com/problems/copy-list-with-random-pointer
+
+// Search a 2D Matrix --- > https://leetcode.com/problems/search-a-2d-matrix
+
+// Maximum Subarray --- > https://leetcode.com/problems/maximum-subarray
+
+// Squares of a Sorted Array --- > https://leetcode.com/problems/squares-of-a-sorted-array
+
+// Min Stack --- > https://leetcode.com/problems/min-stack
+
+// Minimum Window Substring --- > https://leetcode.com/problems/minimum-window-substring
+
+// Binary Tree Right Side View --- > https://leetcode.com/problems/binary-tree-right-side-view
+
+// Keys and Rooms --- > https://leetcode.com/problems/keys-and-rooms
+
+// Course Schedule II --- > https://leetcode.com/problems/course-schedule-ii
+
+// Max Consecutive Ones III --- > https://leetcode.com/problems/max-consecutive-ones-iii
+
+// Flatten Deeply Nested Array --- > https://leetcode.com/problems/flatten-deeply-nested-array
+
+// First Unique Character in a String --- > https://leetcode.com/problems/first-unique-character-in-a-string
+
+// Maximum Profit From Trading Stocks --- > https://leetcode.com/problems/maximum-profit-from-trading-stocks
+
+// Container With Most Water --- > https://leetcode.com/problems/container-with-most-water
+
+// Partition Equal Subset Sum --- > https://leetcode.com/problems/partition-equal-subset-sum
+
+// Merge Two Sorted Lists --- > https://leetcode.com/problems/merge-two-sorted-lists
+
+// Subarray Sum Equals K --- > https://leetcode.com/problems/subarray-sum-equals-k
+
+// Interleaving String --- > https://leetcode.com/problems/interleaving-string
+
+// Boundary of Binary Tree --- > https://leetcode.com/problems/boundary-of-binary-tree
+
+// Path Sum II --- > https://leetcode.com/problems/path-sum-ii
+
+// Populating Next Right Pointers in Each Node --- > https://leetcode.com/problems/populating-next-right-pointers-in-each-node
+
+// Wildcard Matching --- > https://leetcode.com/problems/wildcard-matching
+
+// Course Schedule --- > https://leetcode.com/problems/course-schedule
+
+// Koko Eating Bananas --- > https://leetcode.com/problems/koko-eating-bananas
+
+// Best Time to Buy and Sell Stock II --- > https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+// Jump Game II --- > https://leetcode.com/problems/jump-game-ii
+
+// Palindrome Number --- > https://leetcode.com/problems/palindrome-number
+
+// Valid Palindrome II --- > https://leetcode.com/problems/valid-palindrome-ii
+
+// Boats to Save People --- > https://leetcode.com/problems/boats-to-save-people
+
+// Fruit Into Baskets --- > https://leetcode.com/problems/fruit-into-baskets
+
+// Minimum Add to Make Parentheses Valid --- > https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+
+// Reverse Words in a String III --- > https://leetcode.com/problems/reverse-words-in-a-string-iii
+
+// Reorganize String --- > https://leetcode.com/problems/reorganize-string
+
+// Linked List Cycle --- > https://leetcode.com/problems/linked-list-cycle
+
+// Maximum Number of Non-overlapping Palindrome Substrings --- > https://leetcode.com/problems/maximum-number-of-non-overlapping-palindrome-substrings
+
+// Largest Rectangle in Histogram --- > https://leetcode.com/problems/largest-rectangle-in-histogram
+
+// Reverse Words in a String --- > https://leetcode.com/problems/reverse-words-in-a-string
+
+// Add Two Numbers --- > https://leetcode.com/problems/add-two-numbers
+
+// Second Highest Salary --- > https://leetcode.com/problems/second-highest-salary
+
+// Palindrome Partitioning --- > https://leetcode.com/problems/palindrome-partitioning
+
+// Binary Tree Maximum Path Sum --- > https://leetcode.com/problems/binary-tree-maximum-path-sum
+
+// Count Primes --- > https://leetcode.com/problems/count-primes
+
+// Populating Next Right Pointers in Each Node II --- > https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii
+
+// Maximal Square --- > https://leetcode.com/problems/maximal-square
+
+// Product of Array Except Self --- > https://leetcode.com/problems/product-of-array-except-self
+
+// Valid Anagram --- > https://leetcode.com/problems/valid-anagram
+
+// Perfect Squares --- > https://leetcode.com/problems/perfect-squares
+
+// Binary Tree Longest Consecutive Sequence --- > https://leetcode.com/problems/binary-tree-longest-consecutive-sequence
+
+// Decode Ways --- > https://leetcode.com/problems/decode-ways
+
+// Merge Sorted Array --- > https://leetcode.com/problems/merge-sorted-array
+
+// Longest Palindrome --- > https://leetcode.com/problems/longest-palindrome
+
+// Decode String --- > https://leetcode.com/problems/decode-string
