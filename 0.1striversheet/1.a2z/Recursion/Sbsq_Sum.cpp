61ea3a907bf96f46ae2808c64d20618c30028f00
subham sharrma <subhamsharrma@gmail.com>
2025-11-03 21:50:01 +0530

    add

--- 0.1striversheet/1.a2z/Recursion/Sbsq_Sum.cpp
+++ 0.1striversheet/1.a2z/Recursion/Sbsq_Sum.cpp
@@ -11,37 +11,38 @@
 
 
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Solution {
-// private:
-//     // This method recursively checks for the subsequence with the given sum
-//     bool func(int ind, int sum, std::vector<int> &nums) {
-//         // Base case: if all elements are processed, check if sum is 0
-//         if (ind == nums.size()) {
-//             return sum == 0;
-//         }
-//         // Recursive call: include the current element in the subsequence
-//         // or exclude the current element from the subsequence
-//         return func(ind + 1, sum - nums[ind], nums) | func(ind + 1, sum, nums);
-//     }
 
-// public:
-//     // This method initiates the recursive process
-//     bool checkSubsequenceSum(std::vector<int>& nums, int target) {
-//         return func(0, target, nums); // Start the recursive process
-//     }
-// };
+class Solution {
+private:
+    // This method recursively checks for the subsequence with the given sum
+    bool func(int ind, int sum, std::vector<int> &nums) {
+        // Base case: if all elements are processed, check if sum is 0
+        if (ind == nums.size()) {
+            return sum == 0;
+        }
+        // Recursive call: include the current element in the subsequence
+        // or exclude the current element from the subsequence
+        return func(ind + 1, sum - nums[ind], nums) | func(ind + 1, sum, nums);
+    }
 
-// // Main function to test the solution
-// int main() {
-//     Solution sol;
-//     std::vector<int> nums = {8,9};
-//     int target = 5;
-//     std::cout << sol.checkSubsequenceSum(nums, target); // Expected output: true
-//     return 0;
-// }
+public:
+    // This method initiates the recursive process
+    bool checkSubsequenceSum(std::vector<int>& nums, int target) {
+        return func(0, target, nums); // Start the recursive process
+    }
+};
+
+// Main function to test the solution
+int main() {
+    Solution sol;
+    std::vector<int> nums = {8,9};
+    int target = 5;
+    std::cout << sol.checkSubsequenceSum(nums, target); // Expected output: true
+    return 0;
+}
 
 
  
