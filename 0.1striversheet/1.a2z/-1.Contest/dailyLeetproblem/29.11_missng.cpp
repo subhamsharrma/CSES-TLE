2f49607c07b205630d1e7d9d49b779262f2d5abe
subham sharrma <subhamsharrma@gmail.com>
2025-11-29 23:14:36 +0530

    Restore default tracking behavior

--- 0.1striversheet/1.a2z/-1.Contest/dailyLeetproblem/29.11_missng.cpp
+++ 0.1striversheet/1.a2z/-1.Contest/dailyLeetproblem/29.11_missng.cpp
@@ -1,4 +1,47 @@
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // --------- TYPE SHORTCUTS ----------
+// using ll  = long long;
+// using ld  = long double;
+// using vi  = vector<ll>;
+// using pii = pair<ll,ll>;
+
+// // --------- CONSTANTS ---------------
+// const ll MOD = 1e9 + 7;
+// const ll INF = 1e18;
+
+// // --------- FAST I/O ----------------
+// #define fastio() ios::sync_with_stdio(false); cin.tie(NULL);
+
+// // --------- PRIORITY QUEUES ---------
+// using max_pq = priority_queue<ll>;
+// using min_pq = priority_queue<ll, vector<ll>, greater<ll>>;
+
+// // --------- DEBUG UTILS -------------
+// #ifndef ONLINE_JUDGE
+// #define debug(x) cerr << #x <<" = "; _print(x); cerr << endl;
+// #else
+// #define debug(x)
+// #endif
+
+// void _print(ll x){ cerr << x; }
+// void _print(int x){ cerr << x; }
+// void _print(string x){ cerr << x; }
+// void _print(char x){ cerr << x; }
+// void _print(double x){ cerr << x; }
+// template<class T> void _print(vector<T> v){ cerr << "[ "; for(T i : v){ _print(i); cerr << " "; } cerr << "]"; }
+
+// // -----------------------------------
+
+// int main() {
+//     fastio();
+
+//     // your code here
+
+//     return 0;
+// }
 
 
 
