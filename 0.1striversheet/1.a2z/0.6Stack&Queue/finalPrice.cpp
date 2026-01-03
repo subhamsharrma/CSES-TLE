7e1806998d6ba5cc0e5132afa421d6c4a67620e2
Subham_Sharrma <subhamsharrma@gmail.com>
2026-01-03 12:40:02 +0530

    Added

--- 0.1striversheet/1.a2z/0.6Stack&Queue/finalPrice.cpp
+++ 0.1striversheet/1.a2z/0.6Stack&Queue/finalPrice.cpp
@@ -1,36 +1,66 @@
+// #include <bits/stdc++.h>
+// using namespace std ;
+
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<int> ans = prices;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (prices[j] <= prices[i]) {
+//                     ans[i] = prices[i] - prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// int main() {
+//     Solution sol ;
+//     vector<int> prices = {8,4,6,2,3} ;
+//     vector<int> ans = sol.finalPrices(prices) ;
+//     for(auto x : ans){
+//         cout << x << " " ;
+//     }
+
+//     return 0 ;
+// }
+
+// // Time Complexity: O(n^2)
+// // Space Complexity: O(1)
+// // output :
+// // 4 2 4 2 3
+
+// // Optimized Approach using Stack
+
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
+int main()
+{
+    // let put 0 in the stack
+    stack<int> st;
+    st.push(0);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    stack<int> temp;
 
-class Solution {
-public:
-    vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> ans = prices;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    ans[i] = prices[i] - prices[j];
-                    break;
-                }
-            }
-        }
-        return ans;
+    while (!st.empty())
+    {
+        temp.push(st.top());
+        st.pop();
     }
-};
-
-int main() {
-    Solution sol ; 
-    vector<int> prices = {8,4,6,2,3} ;
-    vector<int> ans = sol.finalPrices(prices) ;
-    for(auto x : ans){
-        cout << x << " " ;
+    while (!temp.empty())
+    {
+        cout << temp.top() << " ";
+        temp.pop();
     }
-
-    return 0 ;
+    return 0;
 }
-
-// Time Complexity: O(n^2)
-// Space Complexity: O(1)
-// output :
-// 4 2 4 2 3
